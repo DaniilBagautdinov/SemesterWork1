e01c74183f584e360ffe907f148c3c0f17188317
Daniil <daniilbagautdinov@yandex.ru>
2021-11-01 22:54:16 +0300

    added script

---
+++ src/main/java/scripts/V4__Create_lessons.sql
@@ -0,0 +1,17 @@
+insert into lessons(title, description, article, link, photo_title)
+values ('ЕГЭ - русский язык','Александр Долгих - научит вас писать и разговаривать',
+        'Как получить 4 балла за 2 минуты?',
+        'https://www.youtube.com/watch?v=kSAhioSzgc8',
+        'Dol.jpg');
+
+insert into lessons(title, description, article, link, photo_title)
+values ('ЕГЭ - математика','Артур Шарафиев - жесткий математик',
+        'Логарифмы за 40 минут',
+        'https://www.youtube.com/watch?v=YiaM3qXlaDo',
+        'Shar.jpg');
+
+insert into lessons(title, description, article, link, photo_title)
+values ('ЕГЭ - информатика','Виктория Ланская - изи инфа',
+        'Легчайшие 2 балла на ЕГЭ - задание №6 и №22',
+        'https://www.youtube.com/watch?v=jKWa_sHakqU',
+        'Lan.jpg');
